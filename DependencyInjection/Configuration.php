4e53dd699b4148591039c8c515400047bf29064c
Kévin Letord <kevin.letord@openclassrooms.com>
2017-06-20 17:43:13 +0200

    Dont perform deepmerging

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -22,6 +22,7 @@ class Configuration implements ConfigurationInterface
         $rootNode
             ->children()
                 ->arrayNode('provider')
+                ->performNoDeepMerging()
                 ->beforeNormalization()
                     ->ifString()
                     ->then(function($v) { return array($v => array()); })
